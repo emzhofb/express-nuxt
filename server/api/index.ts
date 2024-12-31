84fb5893435e6c83d6ad26588186959a9ab4ece6
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2024-12-31 09:58:08 +0700

    add cors and helmet

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const port = 5000;
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(5000, () => console.log('Server ready on port 5000.'));
+app.listen(port, () => console.log('Server ready on port', port));
 
 module.exports = app;
