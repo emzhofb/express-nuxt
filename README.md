a4260382a1488ec7a285c6c97f3bd5028874b6ec
Ikhda Muhammad Wildani <ikhdamuhammad@gmail.com>
2025-01-01 16:28:25 +0700

    update the readme

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ npm run dev
 ```
 
 server
+
+please make the .env file under folder server
+/server/.env
+
+```
+PORT=5000
+MONGO_URI=mongodb://localhost:27017
+MONGO_DATABASE=file-manager
+```
+
+and make sure you have mongodb and create file-manager database with files collection. And then run these command
+
 ```
 cd server
 npm install
--- server/src/database/mongoose.ts
+++ server/src/database/mongoose.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
 // Konfigurasi MongoDB
-const mongoURI = `${process.env.MONGO_URI}/${process.env.MONGO_DATABASE}`;
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017';
 
 export const connectToMongoDB = async () => {
   try {
     // Koneksi ke database target
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURI, {
+      dbName: process.env.MONGO_DATABASE,
+    });
 
-    console.log(`Database "${mongoURI}" connected successfully.`);
+    console.log(`Database connected successfully.`);
   } catch (error) {
     console.error("Error connecting database:", error);
   }
